cdd31c2fff968f0102015be23db1ef835e7b255c
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-03-05 00:30:56 +0530

    error_handling.sol updated

--- contracts/error_handling.sol
+++ contracts/error_handling.sol
@@ -2,16 +2,6 @@
 pragma solidity ^0.8.3;
 
 contract ExampleContract {
-    uint256 public value;
-
-    function setValue(uint256 newValue) public {
-        require(newValue > value, "New value must be greater than current value");
-        value = newValue;
-    }
-
-    function getValue() public view returns (uint256) {
-        return value;
-    }
 
     function add(uint256 a, uint256 b) public pure returns (uint256) {
         uint256 c = a + b;
@@ -20,11 +10,14 @@ contract ExampleContract {
     }
 
     function divide(uint256 a, uint256 b) public pure returns (uint256) {
-        require(b != 0, "Cannot divide by zero");
+        require(b != 0, "Cannot divide by zero as require sayes");
         return a / b;
     }
 
-    function fail() public pure {
-        revert("This function always fails");
+    function fail(uint256 a, uint256 b) public pure returns (uint256){
+        if(b == 0){
+            revert("Cannot divide by zero as revert sayes");
+        }
+        return a / b;
     }
 }
